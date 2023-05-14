e889f236a09db0d9ab2876e5d28407651f1e7251
유지웅 <poujis0@gmail.com>
2023-05-15 03:31:24 +0900

    feat - SignupRequest 구현 및 Controller 적용

--- src/main/java/jw/project/ecommerce/presentation/user/UserController.java
+++ src/main/java/jw/project/ecommerce/presentation/user/UserController.java
@@ -1,17 +1,26 @@
 package jw.project.ecommerce.presentation.user;
 
+import jw.project.ecommerce.application.user.SignupService;
+import jw.project.ecommerce.presentation.user.request.SignupRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/user")
 public class UserController {
+    private final SignupService signupService;
+
     @PostMapping("/signup")
-    public void signup() {
+    public void signup(@Valid @RequestBody SignupRequest request) {
         /**
-         * [ ] Parameter : SignupRequest
-         * [ ] SignupService 구현 및 회원 가입 메소드 호출
+         * [O] Parameter : SignupRequest
+         * [O] SignupService 구현 및 회원 가입 메소드 호출
          * [ ] 공통 Response 구현
          */
+        signupService.signup(request.toCommand());
     }
 
     @PostMapping("/login")
@@ -31,8 +40,9 @@ public class UserController {
          * [ ] 공통 Response 구현
          */
     }
+
     @DeleteMapping("/withdraw")
-    public void withdraw(){
+    public void withdraw() {
         /**
          *  [ ] Parameter : WithdrawRequest
          *  [ ] WithdrawService 구현 및 호출
---
+++ src/main/java/jw/project/ecommerce/presentation/user/request/SignupRequest.java
@@ -0,0 +1,37 @@
+package jw.project.ecommerce.presentation.user.request;
+
+
+import jw.project.ecommerce.application.user.command.SignupCommand;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+public record SignupRequest(
+        /**
+         * [O] Validation 구현
+         *      email - 이메일 형식에 맞는지
+         *            - 빈칸 금지
+         *      password - 빈칸 금지
+         *               - 8글자 ~ 20자
+         *      name :   - 빈칸 금지
+         * [O] toCommand() 구현 : 책임 관계를 명확하게 하기 위함
+         */
+        @NotBlank(message = "아이디를 입력해 주세요")
+        @Email
+        String email,
+        @NotBlank(message = "패스워드를 입력해 주세요")
+        @Size(min = 8, max = 2, message = "8~20글자 사이로 입력해 주세요")
+        String password,
+        @NotBlank(message = "이름을 입력해 주세요")
+        String name
+)
+{
+    public SignupCommand toCommand(){
+        return new SignupCommand(
+                this.email,
+                this.password,
+                this.name
+        );
+    }
+}
